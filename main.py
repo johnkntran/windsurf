667cb69ec6fb7164d809d2e92f66aa46803e396a
johnkntran <john.kn.tran@gmail.com>
2025-07-03 21:40:54 +0000

    Add example for Postgres vector store

--- main.py
+++ main.py
@@ -32,3 +32,62 @@ retriever = store.as_retriever(
     search_kwargs={"k": 1, "fetch_k": 2, "lambda_mult": 0.5},
 )
 docs = retriever.get_relevant_documents("foo")
+
+# ---------------------------------------------------------------------- #
+
+from langchain_postgres.vectorstores import PGVector
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from sqlalchemy.ext.asyncio import create_async_engine
+
+
+connection = "postgresql+psycopg://langchain:langchain@database:5432/langchain"
+collection_name = "my_docs"
+
+vector_store = PGVector(
+    embeddings=OpenAIEmbeddings(),
+    collection_name=collection_name,
+    connection=connection,
+    use_jsonb=True,
+)
+
+document_1 = Document(page_content="foo", metadata={"baz": "bar"})
+document_2 = Document(page_content="thud", metadata={"bar": "baz"})
+document_3 = Document(page_content="i will be deleted :(")
+
+documents = [document_1, document_2, document_3]
+ids = ["1", "2", "3"]
+
+vector_store.add_documents(documents=documents, ids=ids)
+vector_store.delete(ids=["3"])
+
+results = vector_store.similarity_search(query="thud",k=1)
+for doc in results:
+    print(f"* {doc.page_content} [{doc.metadata}]")
+
+results = vector_store.similarity_search(query="thud",k=1,filter={"bar": "baz"})
+for doc in results:
+    print(f"* {doc.page_content} [{doc.metadata}]")
+
+results = vector_store.similarity_search_with_score(query="qux",k=1)
+for doc, score in results:
+    print(f"* [SIM={score:3f}] {doc.page_content} [{doc.metadata}]")
+
+retriever = vector_store.as_retriever(
+    search_type="mmr",
+    search_kwargs={"k": 1, "fetch_k": 2, "lambda_mult": 0.5},
+)
+retriever.invoke("thud")
+
+engine = create_async_engine(connection)
+
+async_vector_store = PGVector(
+    embeddings=OpenAIEmbeddings(),
+    collection_name=collection_name,
+    connection=engine,
+    use_jsonb=True,
+)
+
+results = await async_vector_store.asimilarity_search_with_score(query="qux",k=1)
+for doc,score in results:
+    print(f"* [SIM={score:3f}] {doc.page_content} [{doc.metadata}]")
