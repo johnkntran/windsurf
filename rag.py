d06978b0ad97fa803fed91bcb705814488ae6dde
johnkntran <john.kn.tran@gmail.com>
2025-07-05 00:44:47 +0000

    Refactor with LCEL

--- rag.py
+++ rag.py
@@ -1,24 +1,28 @@
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_openai import ChatOpenAI
-
+from langchain_core.runnables import RunnablePassthrough
 from store import retriever
 
-
-prompt_template = ChatPromptTemplate([
+# Define the prompt template
+prompt_template = ChatPromptTemplate.from_messages([
     ("system", "Use the following context to answer the user's question: {context}"),
     ("user", "{question}")
 ])
 
-question = "What does Mark Carney say about President Trump?"
-
-docs = retriever.invoke(question)
-context = " ".join(d.page_content for d in docs)
-
-messages = prompt_template.invoke({"context": context, "question": question})
-
+# Define the model
 model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-response = model.invoke(messages)
+# Define the chain using LCEL
+chain = (
+    {
+        "context": lambda x: " ".join(d.page_content for d in retriever.invoke(x["question"])),
+        "question": lambda x: x["question"]
+    }
+    | prompt_template
+    | model
+)
 
+# Example usage
+question = "What does Mark Carney say about President Trump?"
+response = chain.invoke({"question": question})
 print(response.content)
