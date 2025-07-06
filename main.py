ec44821a82d39bdc103c89d8d2edbb8e9ec1f9a0
johnkntran <john.kn.tran@gmail.com>
2025-07-06 22:17:09 +0000

    Starter lines

---
+++ main.py
@@ -0,0 +1,8 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+# Load documents
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("Some question about the data should go here")
+print(response)
