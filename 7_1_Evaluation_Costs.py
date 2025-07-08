d222053ff6fb1fa8c44f40d00bd202633731e39c
johnkntran <john.kn.tran@gmail.com>
2025-07-08 17:53:24 +0000

    Update evaluating costs

--- 7_1_Evaluation_Costs.py
+++ 7_1_Evaluation_Costs.py
@@ -46,3 +46,52 @@ Settings.llm = MockLLM(max_tokens=256)
 
 # use a mock embedding globally
 Settings.embed_model = MockEmbedding(embed_dim=1536)
+
+# ---------------------------------------------------------------------------- #
+
+from llama_index.core.llms import MockLLM
+from llama_index.core import MockEmbedding
+from llama_index.core import Settings
+
+llm = MockLLM(max_tokens=256)
+embed_model = MockEmbedding(embed_dim=1536)
+
+import tiktoken
+from llama_index.core.callbacks import CallbackManager, TokenCountingHandler
+
+token_counter = TokenCountingHandler(
+    tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode
+)
+
+callback_manager = CallbackManager([token_counter])
+
+Settings.llm = llm
+Settings.embed_model = embed_model
+Settings.callback_manager = callback_manager
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+documents = SimpleDirectoryReader(
+    "./docs/examples/data/paul_graham"
+).load_data()
+
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+
+response = query_engine.query("query")
+
+print(
+    "Embedding Tokens: ",
+    token_counter.total_embedding_token_count,
+    "\n",
+    "LLM Prompt Tokens: ",
+    token_counter.prompt_llm_token_count,
+    "\n",
+    "LLM Completion Tokens: ",
+    token_counter.completion_llm_token_count,
+    "\n",
+    "Total LLM Token Count: ",
+    token_counter.total_llm_token_count,
+    "\n",
+)
